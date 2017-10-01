f638c69e47c81883ffc340171a2ef31e2ef7b0d0
Chris Vickery <chrisinajar@gmail.com>
2017-10-01 11:35:56 -0700

    Initial implementation of basic horde director (#12)
    * Add basic player watcher and vision detection markers
    
    * Fix some oaa related bugs, better everything
    
    * Init creep watcher, stub out phases in director, build map
    
    * Calculate stress levels of players, very simple right now
    
    * better stress logic, working build up to peak phases
    
    * Only use 1 marker per player
    
    * Start stubbing out horde selector
    
    * Spawning units based on intensity and stress phases
    
    * stress level smoothing, faster marker, enable horde spawning
    
    * Load horde async, never delete a unit within 5 seconds of creation
    
    * Start moving the second a unit spawns
    
    * Enforce max wave time, configure 3-5 minute window

--- game/scripts/vscripts/components/director/director.lua
+++ game/scripts/vscripts/components/director/director.lua
@@ -1,5 +1,122 @@
 HordeDirector = HordeDirector or class({})
 
-function HordeDirector:Init()
+PHASE_BUILD_UP = 1
+PHASE_PEAK = 2
+PHASE_REST = 3
+PHASE_WRAP = 4
 
+local DesiredStressEvent = Event()
+local WaveEvent = Event()
+
+function HordeDirector:Init()
+  DebugPrint('Init hero director')
+
+  self.watchers = {}
+  self.currentPhase = 0
+  self.wave = 1
+  self.timeInWave = 0
+
+  Timers:CreateTimer(1, function()
+    self.timeInWave = self.timeInWave + 1
+    return 1
+  end)
+
+  PlayerResource:GetAllTeamPlayerIDs():each(function (playerID)
+    DebugPrint('Initializing player watcher for player ' .. playerID)
+    local watcher = PlayerWatcher()
+    watcher:Init(playerID)
+    table.insert(self.watchers, watcher)
+
+    DesiredStressEvent.listen(function (ds)
+      watcher.desiredStress = ds
+    end)
+    WaveEvent.listen(function (wave)
+      watcher.wave = wave
+    end)
+  end)
+
+  PeakStressEvent.listen(partial(HordeDirector.OnPeakStress, self))
+
+  -- start horde director
+  HordeSpawner:Init(self)
+  self:EnterNextPhase()
+end
+
+function HordeDirector:OnPeakStress (watcher)
+  DebugPrint('A player just entered peak stress!')
+  if self:AllPlayersInPeakStress() then
+    if self.currentPhase == PHASE_BUILD_UP then
+      self:EnterNextPhase()
+    end
+  end
+end
+
+function HordeDirector:AllPlayersInPeakStress ()
+  for _,watcher in ipairs(self.watchers) do
+    if not watcher:IsPeakStress() then
+      return false
+    end
+  end
+  return true
+end
+
+function HordeDirector:EnterNextPhase()
+  self.currentPhase = self.currentPhase + 1
+  if self.currentPhase == PHASE_WRAP then
+    self.currentPhase = PHASE_BUILD_UP
+  end
+
+  if self.currentPhase == PHASE_BUILD_UP then
+    self:StartBuildUp()
+  elseif self.currentPhase == PHASE_PEAK then
+    self:StartPeak()
+  elseif self.currentPhase == PHASE_REST then
+    self:StartRest()
+  end
+end
+
+function HordeDirector:StartBuildUp()
+  DebugPrint('Entering start up phase')
+  local desiredStress = 0
+  Timers:CreateTimer(1, function()
+    if self.currentPhase ~= PHASE_BUILD_UP then
+      return
+    end
+    desiredStress = math.min(1, desiredStress + 0.01)
+    DesiredStressEvent.broadcast(desiredStress)
+
+    if self.timeInWave > MAX_WAVE_TIME then
+      self:StartNextWave()
+    end
+
+    return 1
+  end)
+end
+
+function HordeDirector:StartPeak()
+  DebugPrint('Entering peak phase')
+  DesiredStressEvent.broadcast(1.1) -- force impossible stress at peak
+  Timers:CreateTimer(10, function()
+    -- end peak on a timer
+    self:EnterNextPhase()
+  end)
+end
+
+function HordeDirector:StartRest()
+  DesiredStressEvent.broadcast(-1)
+  DebugPrint('Entering rest phase')
+  if self.timeInWave > MIN_WAVE_TIME then
+    self:StartNextWave()
+  end
+
+  Timers:CreateTimer(20, function()
+    self:EnterNextPhase()
+  end)
+end
+
+function HordeDirector:StartNextWave()
+  DebugPrint('Starting next wave!')
+  self.timeInWave = 0
+  self.wave = self.wave + 1
+  WaveEvent.broadcast(self.wave)
 end
