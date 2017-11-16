6ff40502f81e0ef683b521b9185ee50e2033c4b9
Chris Vickery <chrisinajar@gmail.com>
2017-11-15 22:20:22 -0800

    remove test code

--- game/scripts/vscripts/components/storyline/manager.lua
+++ game/scripts/vscripts/components/storyline/manager.lua
@@ -8,11 +8,11 @@ function VictoryPhase:Start()
 end
 
 local STORY_STATES = {
-  -- {
-  --   comic = ComicData.act1,
-  --   phase = PhaseOne,
-  --   name = "repair"
-  -- },
+  {
+    comic = ComicData.act1,
+    phase = PhaseOne,
+    name = "repair"
+  },
   {
    comic = ComicData.act2,
    phase = PhaseTwo,
