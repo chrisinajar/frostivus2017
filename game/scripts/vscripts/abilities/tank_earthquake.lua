7a0cafd8083e702b18f6942016984efbe4eea3bc
Yahnich <y.slagmeulder@hotmail.com>
2017-11-16 03:11:29 +0100

    Ebf slam fixes (#114)
    * added tank ai and slam
    
    * fixed class
    
    * updated

--- game/scripts/vscripts/abilities/tank_earthquake.lua
+++ game/scripts/vscripts/abilities/tank_earthquake.lua
@@ -20,6 +20,22 @@ function tank_earthquake:OnSpellStart()
 	local caster = self:GetCaster()
 	local position = self:GetCursorPosition()
 	
-	local targets = 
+	local duration = self:GetSpecialValueFor("stun_duration")
+	local radius = self:GetSpecialValueFor("stun_radius")
+	local damage = self:GetSpecialValueFor("damage")
+	
+	local targets = FindUnitsInRadius( caster:GetTeamNumber(), caster:GetOrigin(), caster, radius, DOTA_UNIT_TARGET_TEAM_ENEMY, DOTA_UNIT_TARGET_HERO + DOTA_UNIT_TARGET_BASIC, 0, 0, false )
 	ParticleManager:ReleaseParticleIndex( self.warningFX )
+	for _, target in pairs( targets ) do
+		target:AddNewModifier(target, self, "modifier_stunned", {duration = duration})
+		ApplyDamage({victim = hero, attacker = self:GetCaster(), damage = damage, ability = self:GetAbility(), damage_type = self:GetAbility():GetAbilityDamageType()})
+	end
+	
+	stunFX = ParticleManager:CreateParticle("particles/units/heroes/hero_leshrac/leshrac_split_earth.vpcf", PATTACH_WORLDORIGIN, nil)
+	ParticleManager:SetParticleControl(stunFX, 0, position)
+	ParticleManager:SetParticleControl(stunFX, 2, position)
+	ParticleManager:SetParticleControl(stunFX, 1, Vector(radius,0,0))
+	ParticleManager:ReleaseParticleIndex( stunFX )
+	EmitSoundOnLocationWithCaster(position, "n_creep_Ursa.Clap", caster)
+
 end
