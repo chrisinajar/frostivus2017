3c61675fd61bb8006559c0e4389221573e6cce2c
Chris Vickery <chrisinajar@gmail.com>
2017-11-18 11:46:24 -0800

    Increase special unit spawn rate

--- game/scripts/vscripts/components/director/director.lua
+++ game/scripts/vscripts/components/director/director.lua
@@ -172,7 +172,7 @@ function HordeDirector:StartBuildUp()
       desiredStress = 0
       return 1
     end
-    local maxRate = 28 - (StorylineManager.currentState * 5)
+    local maxRate = 25 - (StorylineManager.currentState * 5)
     if RandomInt(1, maxRate) == 1 then
       self:SpawnSpecialUnit()
     end
