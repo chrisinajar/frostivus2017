56faa53d68395859cbff665aaa495d2c85901dc1
Nikolai Zimmermann <Chronophylos@users.noreply.github.com>
2017-11-23 22:44:28 +0100

    musci player

--- game/scripts/vscripts/components/music/music_list.lua
+++ game/scripts/vscripts/components/music/music_list.lua
@@ -1,17 +1,17 @@
 -- For music entry: codename, filename, author, length(sec)
 
 MusicList = {
-  [1] = {"Before the Aftermath", "music.background_1", "RyanAKrause", 312},
-  [2] = {"Call to Battle", "music.background_2", "ChrisJCozzi", 312},
-  [3] = {"Counter Strategy", "music.background_3", "RyanAKrause", 311},
-  [4] = {"Requiem", "music.background_4", "RyanAKrause", 387},
-  [5] = {"Soul Returns", "music.background_5", "RyanAKrause", 367},
-  [6] = {"Tides are Turning", "music.background_6", "RyanAKrause", 333},
-  [7] = {"To Be a True Hero", "music.background_7", "JonathanGilmer", 322},
-  [8] = {"Choose Your God", "music.heropick_1", "RyanAKrause"},
-  [9] = {"We Bring the Doom", "music.heropick_2", "RyanAKrause"},
-  [10] = {"Sweat Defeat", "music.defeat", "RyanAKrause"},
-  [11] = {"Nothing but Ashes", "music.win", "RyanAKrause"},
-  [12] = {"Time to Die", "music.duel_intro", "RyanAKrause"},
-  [13] = {"Dueling Minds", "music.duel", "RyanAKrause"}
+  -- Ambient
+  [1] = {"Icy Tranquility", "music.ambient_1", "RyanAKrause"},
+  [2] = {"Summer is not coming", "music.ambient_2", "RyanAKrause"},
+  -- Horror
+  [3] = {"Escape the Cold", "music.horror_1", "RyanAKrause"},
+  [4] = {"Win Just Survive!", "music.horror_2", "RyanAKrause"},
+  -- Christmas
+  [5] = {"Christmass Hordes", "music.christmas_1", "RyanAKrause"},
+  [6] = {"Monster Presents", "music.christmas_2", "RyanAKrause"},
+  -- Combat
+  [7] = {"Monster Presents", "music.combat_2", "RyanAKrause"},
+  -- Tank
+  [8] = {"Tank", "music.tank", "RyanAKrause"},
 }
